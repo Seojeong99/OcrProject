5fcfb79e36c1116fd40ee151619401a7436bea95
Seojeong99 <pinkberry99@naver.com>
2022-01-24 20:24:15 +0900

    22.01.24

--- openYEt.py
+++ openYEt.py
@@ -2,8 +2,9 @@ from PIL import Image
 from pytesseract import *
 
 #traineddata ='kor2'
-filename = "D:\OcrData\가는안상수체_1.JPG"
+#filename = "D:\OcrData\가는안상수체_1.JPG"
+filename = "D:\OcrData2\가나다라마바사아자차카타파하_1.JPG"
 image = Image.open(filename)
-text = image_to_string(image, lang="kor2")
+text = image_to_string(image, lang="kor")
 
 print(text)
