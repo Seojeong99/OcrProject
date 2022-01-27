dbc558c38ebf0d2e054b9a9f523ee4096bbec60b
Seojeong99 <pinkberry99@naver.com>
2022-01-27 20:15:32 +0900

    22.01.27

--- tesseractSim3.py
+++ tesseractSim3.py
@@ -7,28 +7,33 @@ def jaccard_similarity(x, y):
 
     return intersection_cardinality / float(union_cardinality)
 
-f1 = open("D:\OcrData\Original.txt","r",encoding='UTF8')
+f1 = open("C:\\Users\\이서정\\Desktop\\POLARIS LOC\\한글\\sim\\originKorean.txt","r",encoding='UTF8')
+
 origin = f1.read()
 original = list(origin)
 original = ' '.join(original).split()
 #print(original)
 
-f1 = open("D:\OcrData\origin가는kor.txt","r",encoding='UTF8')
+f1 = open("C:\\Users\\이서정\\Desktop\\POLARIS LOC\\한글\\sim\\test1.txt","r",encoding='UTF8')
 t = f1.read()
 thin = list(t)
 thin = ' '.join(thin).split()
-print("가는kor")
+print("1")
 print(jaccard_similarity(original,thin))
 
-f2 = open("D:\OcrData\origin가는kor2.txt","r",encoding='UTF8')
+f2 = open("C:\\Users\\이서정\\Desktop\\POLARIS LOC\\한글\\sim\\test2.txt","r",encoding='UTF8')
 hm = f2.read()
 human = list(hm)
 human = ' '.join(human).split()
-print("가는kor2")
+print("2")
 print(jaccard_similarity(original,human))
 
-
-
+f3 = open("C:\\Users\\이서정\\Desktop\\POLARIS LOC\\한글\\sim\\test3.txt","r",encoding='UTF8')
+ab = f3.read()
+abc = list(ab)
+abc = ' '.join(abc).split()
+print("3")
+print(jaccard_similarity(original,abc))
 
 
 
