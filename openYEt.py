3680aac70e96fcfcdad2ea7112b35a4a42762e38
Seojeong99 <pinkberry99@naver.com>
2022-02-04 18:45:55 +0900

    22.02.04

--- openYEt.py
+++ openYEt.py
@@ -1,10 +1,27 @@
 from PIL import Image
 from pytesseract import *
 
-#traineddata ='kor2'
-#filename = "D:\OcrData\가는안상수체_1.JPG"
-filename = "D:\OcrData2\가나다라마바사아자차카타파하_1.JPG"
+filename = 'C:\\Users\\이서정\\Desktop\\POLARIS LOC\\한글\\sim\\한글 띄어쓰기 없는거_1.jpg'
+#filename ='D:\\OcrData\\가는안상수체_1.jpg'
 image = Image.open(filename)
-text = image_to_string(image, lang="kor")
+text1 = image_to_string(image, lang="new+new4")
+print(text1)
+print('-------------------------\n')
 
-print(text)
+
+text2 = image_to_string(image, lang="kor")
+print(text2)
+print('-------------------------\n')
+
+text3 = image_to_string(image, lang="kor+new4")
+print(text3)
+print('-------------------------\n')
+
+#text3 = image_to_string(image, lang="kor+new")
+#print(text3)
+#print('-------------------------\n')
+
+#config=('-l new+kor --oem 3 --psm 4')
+#text = image_to_string(image, lang="new")
+#text=image_to_string(image, config=config)
+#print(text)
