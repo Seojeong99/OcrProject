46bc1d542039f8f387110f1cea397174484df3fc
Seojeong99 <pinkberry99@naver.com>
2022-02-20 23:33:18 +0900

    22.02.20

--- tesseractSim3.py
+++ tesseractSim3.py
@@ -18,21 +18,21 @@ f1 = open("C:\\Users\\이서정\\Desktop\\POLARIS LOC\\한글\\sim\\test1.txt","
 t = f1.read()
 thin = list(t)
 thin = ' '.join(thin).split()
-print("1")
+print("양재샤넬체")
 print(jaccard_similarity(original,thin))
 
 f2 = open("C:\\Users\\이서정\\Desktop\\POLARIS LOC\\한글\\sim\\test2.txt","r",encoding='UTF8')
 hm = f2.read()
 human = list(hm)
 human = ' '.join(human).split()
-print("2")
+print("가는안상수체")
 print(jaccard_similarity(original,human))
 
 f3 = open("C:\\Users\\이서정\\Desktop\\POLARIS LOC\\한글\\sim\\test3.txt","r",encoding='UTF8')
 ab = f3.read()
 abc = list(ab)
 abc = ' '.join(abc).split()
-print("3")
+print("한컴재즈체")
 print(jaccard_similarity(original,abc))
 
 
