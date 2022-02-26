d5ce1a0d4aeb005533dbc92e13327c99957d68cb
Seojeong99 <pinkberry99@naver.com>
2022-02-26 23:01:54 +0900

    22.02.26

--- testOne.py
+++ testOne.py
@@ -1,11 +1,13 @@
 from PIL import Image
 from pytesseract import *
 
-#traineddata ='kor2'
-filename = "D:\OcrData\한글1.JPG"
+
+filename = "D:\OcrData\한컴 쿨재즈B_1.JPG"
+#filename = "D:\OcrData\가는안상수체_1.JPG"
+#filename = "D:\OcrData2\양재샤넬체_1.JPG"
 #filename = "D:\OcrData2\가나다라마바사아자차카타파하_1.JPG"
 image = Image.open(filename)
 
-text = image_to_string(image, lang="kor")
+text = image_to_string(image, lang="Hancomff")
 
 print(text)
